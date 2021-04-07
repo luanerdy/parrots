543785579a173e6790069ddc151782fdc645e616
luanerdy <luan.vini1@gmail.com>
2021-04-07 07:47:08 -0300

    fim de jogo e reinício

--- assets/js/verifyEndGame.js
+++ assets/js/verifyEndGame.js
@@ -1,11 +1,13 @@
-const flippeds = document.querySelectorAll('.flipped');
-if (flippeds.length === numberOfCards) {
-    setTimeout(() => {
+function verifyEndGame() {
+    const flippeds = document.querySelectorAll('.flipped');
+    if (flippeds.length === numberOfCards) {
+        setTimeout(() => {
 
-        alert(`Você ganhou em ${counter/2} jogadas!`);
-        playAgain = String(prompt('Jogar novamente? (sim/não)'));
-        if(playAgain.toLowerCase() === 'sim') {
-            start();
-        }
-    }, 500);
+            alert(`Você ganhou em ${counter/2} jogadas!`);
+            playAgain = String(prompt('Jogar novamente? (sim/não)'));
+            if(playAgain.toLowerCase() === 'sim') {
+                start();
+            }
+        }, 500);
+    }
 }
