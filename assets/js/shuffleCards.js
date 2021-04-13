87fbf3d46798eb099b8eca0a78456f54347f5b5c
vagnerpavani <vagner.alves.pavani@gmail.com>
2021-04-13 15:42:43 -0300

    preparando review

--- assets/js/shuffleCards.js
+++
@@ -1,9 +0,0 @@
-function shuffleCards(number) {
-
-    const names = ["bobross", "explody", "fiesta", "metal", "revertit", "unicorn", "triplets"];
-    const prechosen = shuffleArray(names);
-    const chosen = prechosen.slice(0, number);
-    const shuffled = shuffleArray(chosen.concat(chosen));
-
-    return shuffled;
-}
